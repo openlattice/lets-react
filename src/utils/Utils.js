c5e62a9e9b2bfa20ad0337dc493fd2f3d5d3040b
Dustin <dustinburns@gmail.com>
2018-06-09 21:39:37 -0700

    another clean up

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -2,18 +2,6 @@
  * @flow
  */
 
-export function getDetails(association :object) {
-  const details = {};
-
-  details.Source = association.src.map(number => entity.find(el => el.id === number));
-  details.Destination = association.dst.map(number => entity.find(el => el.id === number));
-
-  details.Key = association.entityType.key.map(number => property.find(el => el.id === number));
-  details.Properties = association.entityType.properties.map(number => property.find(el => el.id === number));
-
-  return details;
-}
-
 export function randomId() :string {
 
   // https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
