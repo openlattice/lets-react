d92453652e3f44f81446308890e6665727f7e71b
Hristo Oskov <hristo@openlattice.com>
2021-02-09 12:26:34 -0800

    fix test

--- src/containers/app/reducers/initializeApplicationReducer.test.js
+++ src/containers/app/reducers/initializeApplicationReducer.test.js
@@ -1,7 +1,9 @@
-import { Map } from 'immutable';
+import { fromJS } from 'immutable';
 import { RequestStates } from 'redux-reqseq';
 
 import reducer from './initializeApplicationReducer';
+
+import { RS_INITIAL_STATE } from '../../../core/redux/constants';
 import { INITIALIZE_APPLICATION, initializeApplication } from '../actions';
 
 const MOCK_APP_NAME = 'TestApp';
@@ -12,18 +14,11 @@ const MOCK_ERR_RESPONSE = {
   },
 };
 
+const INITIAL_STATE = fromJS({
+  [INITIALIZE_APPLICATION]: RS_INITIAL_STATE,
+});
+
 describe('AppReducer', () => {
-
-  const INITIAL_STATE = reducer(undefined, { type: '__TEST__' });
-
-  test('INITIAL_STATE', () => {
-    expect(INITIAL_STATE).toBeInstanceOf(Map);
-    expect(INITIAL_STATE.toJS()).toEqual({
-      [INITIALIZE_APPLICATION]: {
-        requestState: RequestStates.STANDBY,
-      },
-    });
-  });
 
   describe(INITIALIZE_APPLICATION, () => {
 
